e1733f4c03553d7b869d19a6ba15d5c33d8564bf
Gnagilla <nagillaganesh1@gmail.com>
2023-03-28 13:44:29 +0530

    testing second time

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          react project testing 
+          testing second time
         </a>
       </header>
     </div>
